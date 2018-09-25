1bbe3bf0fa6e36a07f5e17086e66d889db2c5c1e
blmage <bl.mage.fr@gmail.com>
2018-09-25 10:04:34 +0200

    Create Zend_Filter_Input instances directly
    The code generated by 2.1.x versions for `Zend_Filter_InputFactory` contains a syntax error

--- Controller/Adminhtml/Shipping/Method/Rule/Save.php
+++ Controller/Adminhtml/Shipping/Method/Rule/Save.php
@@ -13,17 +13,11 @@ use ShoppingFeed\Manager\Api\Shipping\Method\RuleRepositoryInterface;
 use ShoppingFeed\Manager\Controller\Adminhtml\Shipping\Method\RuleAction;
 use ShoppingFeed\Manager\Model\Shipping\Method\RuleFactory;
 use ShoppingFeed\Manager\Ui\DataProvider\Shipping\Method\Rule\Form\DataProvider as RuleFormDataProvider;
-use Zend_Filter_InputFactory as InputFilterFactory;
 
 
 class Save extends RuleAction
 {
     const ADMIN_RESOURCE = 'ShoppingFeed_Manager::shipping_method_rule_edit';
-
-    /**
-     * @var InputFilterFactory
-     */
-    private $inputFilterFactory;
 
     /**
      * @var DateFilterFactory
@@ -37,7 +31,6 @@ class Save extends RuleAction
      * @param PageResultFactory $pageResultFactory
      * @param RuleFactory $ruleFactory
      * @param RuleRepositoryInterface $ruleRepository
-     * @param InputFilterFactory $inputFilterFactory
      * @param DateFilterFactory $dateFilterFactory
      */
     public function __construct(
@@ -47,10 +40,8 @@ class Save extends RuleAction
         PageResultFactory $pageResultFactory,
         RuleFactory $ruleFactory,
         RuleRepositoryInterface $ruleRepository,
-        InputFilterFactory $inputFilterFactory,
         DateFilterFactory $dateFilterFactory
     ) {
-        $this->inputFilterFactory = $inputFilterFactory;
         $this->dateFilterFactory = $dateFilterFactory;
 
         parent::__construct(
@@ -98,31 +89,29 @@ class Save extends RuleAction
             $applierConfigData = (array) ($applierData[$applierCode] ?? []);
 
             $dateFilter = $this->dateFilterFactory->create();
-            $inputFilter = $this->inputFilterFactory->create(
+            $inputFilter = new \Zend_Filter_Input(
+                array_intersect_key(
+                    [
+                        RuleFormDataProvider::FIELD_FROM_DATE => $dateFilter,
+                        RuleFormDataProvider::FIELD_TO_DATE => $dateFilter,
+                    ],
+                    array_filter($ruleData)
+                ),
                 [
-                    'filterRules' => array_intersect_key(
-                        [
-                            RuleFormDataProvider::FIELD_FROM_DATE => $dateFilter,
-                            RuleFormDataProvider::FIELD_TO_DATE => $dateFilter,
-                        ],
-                        array_filter($ruleData)
-                    ),
-                    'validatorRules' => [
-                        RuleFormDataProvider::FIELD_NAME => 'NotEmpty',
-                        RuleFormDataProvider::FIELD_DESCRIPTION => [],
-                        RuleFormDataProvider::FIELD_IS_ACTIVE => [],
-                        RuleFormDataProvider::FIELD_FROM_DATE => [],
-                        RuleFormDataProvider::FIELD_TO_DATE => [],
-                        RuleFormDataProvider::FIELD_SORT_ORDER => 'NotEmpty',
-                        RuleFormDataProvider::FIELD_CONDITIONS => [
-                            \Zend_Filter_Input::PRESENCE => \Zend_Filter_Input::PRESENCE_OPTIONAL,
-                        ],
+                    RuleFormDataProvider::FIELD_NAME => 'NotEmpty',
+                    RuleFormDataProvider::FIELD_DESCRIPTION => [],
+                    RuleFormDataProvider::FIELD_IS_ACTIVE => [],
+                    RuleFormDataProvider::FIELD_FROM_DATE => [],
+                    RuleFormDataProvider::FIELD_TO_DATE => [],
+                    RuleFormDataProvider::FIELD_SORT_ORDER => 'NotEmpty',
+                    RuleFormDataProvider::FIELD_CONDITIONS => [
+                        \Zend_Filter_Input::PRESENCE => \Zend_Filter_Input::PRESENCE_OPTIONAL,
                     ],
-                    'options' => [
-                        \Zend_Filter_Input::ALLOW_EMPTY => true,
-                        \Zend_Filter_Input::PRESENCE => \Zend_Filter_Input::PRESENCE_REQUIRED,
-                    ],
-                    'data' => $ruleData,
+                ],
+                $ruleData,
+                [
+                    \Zend_Filter_Input::ALLOW_EMPTY => true,
+                    \Zend_Filter_Input::PRESENCE => \Zend_Filter_Input::PRESENCE_REQUIRED,
                 ]
             );
 
